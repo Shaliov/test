ecdc5650d81c7252c38ade7dfcf87777b514598f
Shaliov Andrey <Andrey.Shaliov@gmail.com>
2017-10-03 11:09:24 +0300

    Настройка spring и настройка log4j

---
+++ src/main/java/com/inventain/test/config/AppConfig.java
@@ -0,0 +1,27 @@
+package com.inventain.test.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+/**
+ * @author Andrey
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.inventain.test"})
+public class AppConfig {
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+
+}
